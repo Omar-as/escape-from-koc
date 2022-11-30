6193b775bd20149c1e2a16ca25c95317f7099a0c
myildirim19 <myildirim19@ku.edu.tr>
2022-11-30 19:51:59 +0300

    Object Class created

---
+++ src/Obj.java
@@ -0,0 +1,11 @@
+public class Obj {
+    private int xPosition;
+    private int yPosition;
+    private String type;
+
+    public Obj(int xPosition, int yPosition, String type) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.type = type;
+    }
+}
