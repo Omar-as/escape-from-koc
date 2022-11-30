ca039e3fb8836316a5c29075d48fe36639ba3e6c
Ameer Taweel <ameertaweel2002@gmail.com>
2022-11-30 22:55:44 +0300

    refactor: Alien class
    Used the new Position utility class.

---
+++ src/models/alien/Alien.java
@@ -0,0 +1,33 @@
+package models.alien;
+
+import utils.Position;
+
+public class Alien {
+    private final AlienType type;
+    private Position position;
+
+    public Alien(AlienType type, int xPosition, int yPosition) {
+        this.type = type;
+        this.position = new Position(xPosition, yPosition);
+    }
+
+    public AlienType getType() {
+        return type;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(int xPosition, int yPosition) {
+        this.position = new Position(xPosition, yPosition);
+    }
+
+    public void setXPosition(int xPosition) {
+        this.position = new Position(xPosition, this.position.getY());
+    }
+
+    public void setYPosition(int yPosition) {
+        this.position = new Position(this.position.getX(), yPosition);
+    }
+}
