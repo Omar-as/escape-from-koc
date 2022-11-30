ec7cd1fbd2d8b8fefd98e468f80a8d78d279e220
Ameer Taweel <ameertaweel2002@gmail.com>
2022-11-30 22:54:54 +0300

    feat: create position utility class

---
+++ src/utils/Position.java
@@ -0,0 +1,22 @@
+package utils;
+
+/**
+ * Immutable 2D Position
+ */
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
