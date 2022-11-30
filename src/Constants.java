58d5a07c8a4c5e9056abe704ad9c8315b894d8f8
Ameer Taweel <ameertaweel2002@gmail.com>
2022-12-01 00:21:04 +0300

    feat: create Constants class

---
+++ src/Constants.java
@@ -0,0 +1,8 @@
+public final class Constants {
+    // This class should not be instanced
+    private Constants() { }
+
+    public static final int FRAME_WIDTH  = 1024;
+    public static final int FRAME_HEIGHT = 768;
+    public static final String FRAME_TITLE = "Escape From Koç";
+}
