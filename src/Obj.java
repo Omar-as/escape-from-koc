a2b755e3792dd4e41c2847ec7266864c7d299ca3
myildirim19 <myildirim19@ku.edu.tr>
2022-11-30 20:00:51 +0300

    setters and getters added

--- src/Obj.java
+++ src/Obj.java
@@ -3,6 +3,30 @@ public class Obj {
     private int yPosition;
     private String type;
 
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public String getType() {
+        return type;
+    }
+
     public Obj(int xPosition, int yPosition, String type) {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
