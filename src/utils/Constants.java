b75253093590bfbaac90753e19f73ba1cbbd21f3
Ameer Taweel <ameertaweel2002@gmail.com>
2022-12-01 02:07:41 +0300

    feat: add fps

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -1,3 +1,5 @@
+package utils;
+
 public final class Constants {
     // This class should not be instanced
     private Constants() { }
@@ -5,4 +7,7 @@ public final class Constants {
     public static final int FRAME_WIDTH  = 1024;
     public static final int FRAME_HEIGHT = 768;
     public static final String FRAME_TITLE = "Escape From Koç";
+
+    // Frame Rate: 30 FPS
+    public static final long REPAINT_DELAY_MILLS = 33;
 }
