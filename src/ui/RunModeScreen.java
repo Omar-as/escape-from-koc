fe39be30fd77a205fb6109074891570827d80f17
omar-as <omarelassaad93@gmail.com>
2022-12-08 20:56:11 +0300

    Added Door UI

--- src/ui/RunModeScreen.java
+++ src/ui/RunModeScreen.java
@@ -2,6 +2,7 @@ package ui;
 
 import control.Backend;
 import models.Game;
+import models.Key;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -14,10 +15,21 @@ public class RunModeScreen extends AnimatedScreen<Game> {
     BufferedImage playerBufferedImage;
     Image playerImage = null;
 
+    BufferedImage closedDoorBufferedImage;
+    Image closedDoorImage = null;
+
+    BufferedImage openedDoorBufferedImage;
+    Image openedDoorImage = null;
+
+
     public RunModeScreen(Game state, Backend<Game> backend) throws IOException {
         super(state, backend);
         // Load assets
         playerBufferedImage = ImageIO.read(new File("assets/player/player_art.png"));
+        closedDoorBufferedImage = ImageIO.read(new File("assets/door/closed_door.png"));
+        openedDoorBufferedImage = ImageIO.read(new File("assets/door/opened_door.png"));
+
+
     }
 
     @Override
@@ -26,12 +38,32 @@ public class RunModeScreen extends AnimatedScreen<Game> {
         int height = getHeight();
         var player = state.getPlayer();
 
-        // Scale images
-        playerImage = playerBufferedImage.getScaledInstance(player.getWidth(), player.getHeight(), Image.SCALE_FAST);
-
         canvas.clearRect(0, 0, width, height); // Clear entire canvas
 
         canvas.setColor(Color.BLACK);
+
+        // Scale images
+        playerImage = playerBufferedImage.getScaledInstance(player.getWidth(), player.getHeight(), Image.SCALE_FAST);
+        closedDoorImage = closedDoorBufferedImage.getScaledInstance(
+                state.getRooms()[0].getDoor().getWidth(),
+                state.getRooms()[0].getDoor().getHeight(),Image.SCALE_DEFAULT);
+        openedDoorImage = openedDoorBufferedImage.getScaledInstance(
+                state.getRooms()[0].getDoor().getWidth(),
+                state.getRooms()[0].getDoor().getHeight(),Image.SCALE_DEFAULT);
+
+        if(state.getRooms()[0].getKey().isFound()){
+            canvas.drawImage(openedDoorImage,
+                    width - state.getRooms()[0].getDoor().getWidth(),
+                    height - state.getRooms()[0].getDoor().getHeight(),
+                    null);
+        }else{
+            canvas.drawImage(closedDoorImage,
+                    width - state.getRooms()[0].getDoor().getWidth(),
+                    height - state.getRooms()[0].getDoor().getHeight(),
+                    null);
+        }
+
+
 
         canvas.drawImage(playerImage, player.getPosition().getX(), player.getPosition().getY(), null);
     }
