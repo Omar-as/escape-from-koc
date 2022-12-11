a0c94af6a7d5f439f652db54cb568c0d8867f4db
Ameer Taweel <ameertaweel2002@gmail.com>
2022-12-11 16:36:45 +0300

    refactor: main menu

---
+++ src/ui/screens/MainMenu.java
@@ -0,0 +1,55 @@
+package ui.screens;
+
+import control.BuildModeBackend;
+import models.BuildModeState;
+import ui.Screen;
+import ui.ScreenManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.stream.Stream;
+
+public class MainMenu extends Screen {
+    public MainMenu() {
+        this.setLayout(new GridBagLayout());
+
+        var mainColumn = Box.createVerticalBox();
+        this.add(mainColumn);
+
+        var title = new JLabel("Main Screen");
+        // TODO: Remove magic numbers
+        title.setFont(new Font("Arial", Font.PLAIN, 40));
+        title.setAlignmentX(CENTER_ALIGNMENT);
+        mainColumn.add(title);
+
+        var mainMenu = new JPanel();
+        mainMenu.setLayout(new GridLayout(0, 1));
+        addMainMenuButton(mainMenu, "Play Game", e -> {
+            // Launch Build Mode
+            var backend = new BuildModeBackend();
+            var screen  = new BuildModeScreen(new BuildModeState(Stream.of(
+                    "Student Center",
+                    "CASE Building",
+                    "SOS Building",
+                    "SCI Building",
+                    "ENG Building",
+                    "SNA Building"
+            )), backend);
+            ScreenManager.getInstance().setScreen(screen);
+        });
+        addMainMenuButton(mainMenu, "Credits", e -> {});
+        addMainMenuButton(mainMenu, "Options", e -> {});
+        addMainMenuButton(mainMenu, "Game History", e -> {});
+        mainColumn.add(mainMenu);
+    }
+
+    private void addMainMenuButton(JComponent mainMenu, String text, ActionListener listener) {
+        var btn = new JButton(text);
+        // TODO: Set uniform style
+        // btn.setBackground(Color.GRAY);
+        // btn.setForeground(Color.BLACK);
+        btn.addActionListener(listener);
+        mainMenu.add(btn);
+    }
+}
