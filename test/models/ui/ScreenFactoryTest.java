e472635bf99360c7fa3725bd406cb285120eea55
omar-as <omarelassaad93@gmail.com>
2022-12-28 20:02:54 +0300

    created ScreenFactoryTest class

---
+++ test/models/ui/ScreenFactoryTest.java
@@ -0,0 +1,14 @@
+package ui;
+
+import com.sun.tools.javac.Main;
+import ui.screens.MainScreen;
+import utils.Constants;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScreenFactoryTest {
+
+    @org.junit.jupiter.api.Test
+     void getScreen() {
+    }
+}
