718fef8ad1388ca384c4aa55e0ae6315a14209e7
Ameer Taweel <ameertaweel2002@gmail.com>
2023-01-21 11:17:28 +0300

    refactor: number of top scores

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -38,7 +38,7 @@ public final class Constants {
     // MISC
     public static final String HOME_DIRECTORY = System.getProperty("user.home");
     public static final int SECOND_MILLS = 1000;
-    public static final int SCOREBOARD_MAX = 10;
+    public static final int SCOREBOARD_MAX = 5;
 
     // This class should not be instanced
     private Constants() {
